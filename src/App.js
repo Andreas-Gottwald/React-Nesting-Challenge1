d2cbe64cf0a61093e5220740eeacf6d8abb7bdcd
Andreas Gottwald <andreas.gottwald@hotmail.de>
2023-12-27 17:57:17 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
+  function handleClick() {
+    console.log("Pfui");
+  }
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>Button 1</Button>
+      <Button>Button 2</Button>
+      <Button>Button 3</Button>
+      <Button onClick={handleClick}>Button 4</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children, onClick }) {
   return (
-    <button className="button" type="button">
-      Click me!
+    <button className="button" type="button" onClick={onClick}>
+      {children}
     </button>
   );
 }
